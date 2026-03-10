84fa4e1d12ceab4200c16597af42f1bccce8438c
Apjesk <sridharsundaram85@gmail.com>
2026-03-09 23:57:30 -0500

    test
    test

--- main.tf
+++
@@ -1,7 +0,0 @@
-resource "databricks_catalog" "this" {
-  name         = var.catalog_name
-  comment      = var.catalog_comment
-  storage_root = var.storage_root
-  owner        = var.catalog_owner
-  force_destroy = var.force_destroy
-}
--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "catalog_id" {
-  description = "Catalog identifier"
-  value       = databricks_catalog.this.id
-}
-
-output "catalog_name" {
-  description = "Created catalog name"
-  value       = databricks_catalog.this.name
-}
